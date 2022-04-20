3224f56720d805ade7ad10bd4a85b601426307de
Cartafina <camila.cartafina@kellogg.northwestern.edu>
2022-04-20 15:52:59 +0000

    change actor_id

--- kmdb.sql
+++ kmdb.sql
@@ -118,7 +118,8 @@ CREATE TABLE studios (
     studio TEXT
 );
 CREATE TABLE top_casts (
-    actor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_id INTEGER,
     actor_name TEXT,
     character TEXT,
     movie_title TEXT,
@@ -176,166 +177,196 @@ INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "Batman Begins",
     "Christian Bale",
     "Bruce Wayne",
+    1,
     1
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "Batman Begins",
     "Michael Caine",
     "Alfred",
-    1
+    1,
+    2
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "Batman Begins",
     "Liam Neeson",
     "Ra's Al Ghul",
-    1
+    1,
+    3
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "Batman Begins",
     "Katie Holmes",
     "Rachel Dawes",
-    1
+    1,
+    4
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "Batman Begins",
     "Gary Oldman",
     "Commissioner Gordon",
+    1,
+    5
+);
+INSERT INTO top_casts (
+    movie_title,
+    actor_name,
+    character,
+    movie_id,
+    actor_id
+) VALUES (
+    "The Dark Knight",
+    "Christian Bale",
+    "Bruce Wayne",
+    2,
     1
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
-) VALUES (
-    "The Dark Knight",
-    "Christian Bale",
-    "Bruce Wayne",
-    2
-);
-INSERT INTO top_casts (
-    movie_title,
-    actor_name,
-    character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight",
     "Heath Ledger",
     "Joker",
-    2
+    2,
+    6
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight",
     "Aaron Eckhart",
     "Harvey Dent",
-    2
+    2,
+    7
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight",
     "Michael Caine",
     "Alfred",
+    2,
     2
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight",
     "Maggie Gyllenhaal",
     "Rachel Dawes",
-    2
+    2,
+    8
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight Rises",
     "Christian Bale",
     "Bruce Wayne",
-    3
+    3,
+    1
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight Rises",
     "Gary Oldman",
     "Commissioner Gordon",
-    3
+    3,
+    5
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight Rises",
     "Tom Hardy",
     "Bane",
-    3
+    3,
+    9
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight Rises",
     "Joseph Gordon-Levitt",
     "John Blake",
-    3
+    3,
+    10
 );
 INSERT INTO top_casts (
     movie_title,
     actor_name,
     character,
-    movie_id
+    movie_id,
+    actor_id
 ) VALUES (
     "The Dark Knight Rises",
     "Anne Hathaway",
     "Selina Kyle",
-    3
+    3,
+    11
 );
 
 -- Prints a header for the movies output
